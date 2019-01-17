10d1f24fe70207efcf58b8e8c9593a48beeb2d25
Clarice Costello <claricostello@outlook.com>
2019-01-17 09:54:52 -0800

    intial commit

---
+++ LINQapp/LINQapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
